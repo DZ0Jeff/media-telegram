0223caad29022d67f6257fd6f8fcd8807d57d0e0
Jeff <mx.jeferson.12@gmail.com>
2021-04-14 13:15:12 -0300

    update media functions

--- app.py
+++ app.py
@@ -2,28 +2,33 @@ from telethon import TelegramClient
 from dotenv import load_dotenv
 import os
 
-load_dotenv()
+def TelegramBot():  
+    # Login
+    load_dotenv()
+    api_id = os.environ.get('API_ID')
+    api_hash = os.environ.get('API_HASH')
 
-api_id = os.environ.get('API_ID')
-api_hash = os.environ.get('API_HASH')
+    bot_name = 'telework'
 
-bot_name = 'telework'
+    try:
+        client = TelegramClient(bot_name, api_id, api_hash)
+        client.start()
+        print(f'[{bot_name}] Logado com sucesso!')
 
-try:
-    client = TelegramClient(bot_name, api_id, api_hash)
-    client.start()
-    print(f'[{bot_name}] Logado com sucesso!')
-
-except Exception as error:
-    print(f'[{bot_name}] Um erro aconteceu...')
-    print(error)
-
-
-async def main():
-    print(f"[{bot_name}] A enviar arquivos...")
-    await client.send_file("me", "Pearl Jam - 15 - Black.mp3")
-    print(f"[{bot_name}] Finalizado!")
+    except Exception as error:
+        print(f'[{bot_name}] Um erro aconteceu...')
+        print(error)
 
 
-with client:
-    client.loop.run_until_complete(main())
+    async def send_media():
+        # envio de arquivos
+        target_file = str(input('Nome do arquivo: ')).strip()
+        print(f"[{bot_name}] A enviar arquivos...")
+        await client.send_file("me", target_file)
+        print(f"[{bot_name}] Finalizado!")
+
+
+    with client:
+        client.loop.run_until_complete(send_media())
+
+TelegramBot()
